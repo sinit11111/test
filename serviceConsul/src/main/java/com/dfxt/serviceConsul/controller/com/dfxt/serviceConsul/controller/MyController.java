7bb6438ee190d0268de188cffb25b04076d15d13
838314506 <liuzhenzhen_2016@163.com>
2018-09-19 16:48:43 +0800

    加入一个mycontroller

---
+++ serviceConsul/src/main/java/com/dfxt/serviceConsul/controller/com/dfxt/serviceConsul/controller/MyController.java
@@ -0,0 +1,7 @@
+package com.dfxt.serviceConsul.controller.com.dfxt.serviceConsul.controller;
+
+public class MyController {
+	public static void main(String[] args) {
+		System.out.println("好的！");
+	}
+}
